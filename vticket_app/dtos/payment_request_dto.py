b65e35e8ac8584ca911f8f2d89202e3fedabb0b9
thuannt thuannt <thuannt@vnpay.vn>
2024-05-30 15:38:01 +0700

    update: add order id

--- vticket_app/dtos/payment_request_dto.py
+++ vticket_app/dtos/payment_request_dto.py
@@ -4,6 +4,7 @@ from datetime import datetime
 @dataclass
 class PaymentRequestDTO():
     amount: int = None
+    order_id: int = None
     created_date: datetime = None
     customer_ip: str = ""
     locale: str = ""
