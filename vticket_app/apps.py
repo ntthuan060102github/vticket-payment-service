05229b62ce772d8644ee184b2ba228a9dbe6cc12
tuyetnhung2001 <Tuyetnhung.mine@gmail.com>
2024-06-05 22:32:03 +0700

    rollback

--- vticket_app/apps.py
+++ vticket_app/apps.py
@@ -1,14 +1,4 @@
-import threading
-import time
 from django.apps import AppConfig
-
-def keep_alive():
-    while True:
-        print("keep alive!")
-        time.sleep(5)
-
-keep_app_alive_thread = threading.Thread(target=keep_alive)
-keep_app_alive_thread.start()
 
 class VticketAccountServiceConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
