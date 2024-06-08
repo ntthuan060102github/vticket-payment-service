fc8b247646820cddafadf0ee61099ea5ce0f2112
ntthuan060102github <ntthuan060102.work@gmail.com>
2024-06-08 22:27:35 +0700

    feat: refund

--- vticket_app/services/payment_service.py
+++ vticket_app/services/payment_service.py
@@ -1,4 +1,8 @@
+import hashlib
+import hmac
 import json
+import socket
+import pytz
 import requests
 from uuid import uuid4
 from datetime import datetime
@@ -15,8 +19,10 @@ class PaymentService():
     __terminal_id = settings.PAYMENT_TERMINAL_ID
     __hash_secret = settings.PAYMENT_HASH_SECRET
     __base_url = settings.PAYMENT_URL
+    __refund_url = settings.PAYMENT_REFUND_URL
     __version = settings.PAYMENT_VERSION
     __pay_command = settings.PAYMENT_PAY_COMMAMD
+    __refund_command = settings.PAYMENT_REFUND_COMMAMD
 
     def get_payment_url(self, data: PaymentRequestDTO) -> str:
         vnp = vnpay()
@@ -90,3 +96,48 @@ class PaymentService():
         except Exception as e:
             print(e)
             return False
+        
+    def refund(self, user_id: int, payment: Payment) -> bool:
+        try:
+            data = {
+                "vnp_RequestId": uuid4().hex,
+                "vnp_Version": self.__version,
+                "vnp_Command": self.__refund_command,
+                "vnp_TmnCode": self.__terminal_id,
+                "vnp_TransactionType": "02",
+                "vnp_TxnRef": payment.payment_request.id.hex,
+                "vnp_Amount": int(payment.amount/100),
+                "vnp_TransactionNo": payment.tran_no,
+                "vnp_TransactionDate": payment.pay_date.strftime("%Y%m%d%H%M%S"),
+                "vnp_CreateBy": f"user_{user_id}",
+                "vnp_CreateDate": datetime.now(pytz.timezone("Asia/Ho_Chi_Minh")).strftime("%Y%m%d%H%M%S"),
+                "vnp_IpAddr": self.get_machine_ip(),
+                "vnp_OrderInfo": f"Hoan_tien_don_hang_{payment.payment_request.id}"
+            }
+
+            pre_checksum_data = "|".join([str(v) for v in data.values()])
+            secure_hash = hmac.new(self.__hash_secret.encode(), pre_checksum_data.encode(), hashlib.sha512).hexdigest()
+            data["vnp_SecureHash"] = secure_hash
+            resp = requests.post(
+                url=self.__refund_url,
+                data=json.dumps(data),
+                headers={"Content-Type": "application/json"}
+            )
+            resp_data = resp.json()
+
+            return resp_data["vnp_ResponseCode"] == "00"
+        except Exception as e:
+            print(e)
+            return False
+
+    def __hash_with_secure_type(self, secret_key: str, data: str) -> str:
+        return vnpay._vnpay__hmacsha512(secret_key, data=data)
+    
+    def get_machine_ip(self):
+        try:
+            hostname = socket.gethostname()
+            ip_address = socket.gethostbyname(hostname)
+            return ip_address
+        except Exception as e:
+            print(e)
+            return None
