bf3c01c3a8736f55434d92f4c07c5d3e933512c6
ntthuan060102github <ntthuan060102.work@gmail.com>
2024-06-12 23:48:21 +0700

    add amqp conn name

--- vticket_app/configs/amqp.py
+++ vticket_app/configs/amqp.py
@@ -1,6 +1,10 @@
 import pika
 from django.conf import settings
 
-__amqp_conn = pika.BlockingConnection(pika.URLParameters(settings.AMQP_URL))
+url_params = pika.URLParameters(settings.AMQP_URL)
+url_params.client_properties = {
+    "connection_name": "payment_service"
+}
+__amqp_conn = pika.BlockingConnection(url_params)
 log_channel = __amqp_conn.channel()
 log_channel.queue_declare(queue='system_log')
